846f1c81fb2f5bb5cbc10967c08b5974a47716e1
Luzzo17 <ferrucciangelo03@gmail.com>
2026-08-29 21:08:36 +0200

    Controls

---
+++ timer-app/src/components/TimerControls.jsx
@@ -0,0 +1,35 @@
+import { FaPlay, FaPause, FaArrowsRotate } from "react-icons/fa6";
+
+export default function TimerControls({
+    isRunning,
+    secondsLeft,
+    onStart,
+    onPause,
+    onReset,
+}) {
+    return (
+        <div className="timer-controls">
+            {!isRunning ? (
+                <button className="primary-button"
+                onClick={onStart}
+                disabled={secondsLeft === 0}
+                >
+                    <FaPlay /> Avvia
+                </button>
+            ) : (
+                <button className= "primary-button"
+                onClick={onPause}
+                >
+                    <FaPause /> Pausa
+                </button>
+            )
+        
+        }
+                <button className="secondary-button"
+                onClick={onReset}
+                >
+                    <FaArrowsRotate /> Reset
+                </button>
+        </div>
+    )
+}
